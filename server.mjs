db24e241dea80270ecd6ef0404363b91c52174ef
Mikail <maazfarooqi@hotmail.com>
2023-11-02 11:49:08 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
-import fetch from 'node-fetch';
-const cors = import('cors');
+const fetch = require('node-fetch');
+const cors = require('cors');
 
 const app = express();
 const PORT = 4000; // Choose any port number you prefer
